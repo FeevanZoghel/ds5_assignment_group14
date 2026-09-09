8e68b5d57d6bc4c341658b64d6680c6a63112203
MatthijsvanMeeuwen <579720@student.fontys.nl>
2026-09-09 16:49:03 +0200

    limiet check complex getal
    kijkt per complexe getal (in dit geval nog maar 1) of het convergeert of divergeert

--- Week 2/Opdracht2.py
+++ Week 2/Opdracht2.py
@@ -48,6 +48,37 @@ def complex(x,y):
 
 
 
+complex_getal = 1 + 0j
+
+def complex_limiet(complex_getal)->tuple()
+    '''
+    Input: een complex getal, 
+    Kijkt per getal of het convergeert of divergeert en voegt het daarna aan een lijst toe
+    Output: 1 tuple met 2 lijsten erin, 
+    - één met alle geconvergeerde getallen (tot nu toe nog 1)
+    - één met alle gedivergeerde getallen
+
+    '''
+    diverges = []
+    convergers = []
+    A0 = 0 
+    for i in range(1,100):
+        if i==1:
+            An = A0 + complex_getal
+        else: 
+            An = (An)**2 + complex_getal
+        if abs(An)>2:
+            diverges.append(complex_getal)
+            break
+    if abs(An)<=2:   
+        convergers.append(complex_getal)
+    return diverges, convergers
+
+
+
+
+
+
 
 
 

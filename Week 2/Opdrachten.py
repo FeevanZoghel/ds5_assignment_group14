8fd03db4cf84fe8b0f29feaff6de01efa0422ddf
Julien784543 <575068@student.fontys.nl>
2026-09-09 14:54:51 +0200

    Update Opdrachten.py

--- Week 2/Opdrachten.py
+++ Week 2/Opdrachten.py
@@ -28,6 +28,9 @@ def print_records(filtered_records):
     Print de naam en grade van elk record in een lijst
 
     Returns: print alleen de records en geeft geen waarde
+
+    Julien 
+    
     """
     for record in filtered_records:
         print(f"Name: {record['Name']}")

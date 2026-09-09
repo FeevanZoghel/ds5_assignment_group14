bc8830c11b457206a6702f917e2957db6e0027bf
FeevanZoghel <582755@student.fontys.nl>
2026-09-09 15:27:30 +0200

    Update Opdrachten.py

--- Week 2/Opdrachten.py
+++ Week 2/Opdrachten.py
@@ -37,8 +37,7 @@ def Excellent_records(records):
     Kijkt of een cijfer hoger is dan een 80.0 en filterd die eruit
     """
     filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
-    return filtered_records
-
+    
     """
     Kijkt of een cijfer hoger is dan een 80.0 en filterd die eruit
     
@@ -53,5 +52,8 @@ def Excellent_records(records):
         print(f"Name: {record['Name']}")
         print(f"Grade: {record['Grade']}")
         print("--------------------")
+
+    return filtered_records
+    
 
 

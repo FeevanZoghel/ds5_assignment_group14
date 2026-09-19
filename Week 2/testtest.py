23ca54db62bed30dd10e1bd1f93a21ee2e5a5fc9
MatthijsvanMeeuwen <579720@student.fontys.nl>
2026-09-19 20:33:10 +0200

    beetje geprutst

--- Week 2/testtest.py
+++ Week 2/testtest.py
@@ -72,5 +72,56 @@ def create_network(k : int, N: int, M: int) -> nx.Graph:
 
 G = create_network(5, 20, 2)
 
-nx.draw(G, with_labels=True)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+pagerank = nx.pagerank(G)
+
+sorted_pagerank = sorted(
+    pagerank.items(),
+    key=lambda x: x[1],
+    reverse=True
+)
+
+hoogste = sorted_pagerank[0][1]
+
+x = [0]
+y = [0]
+values = [hoogste]
+
+for node, value in sorted_pagerank[1:]:
+
+    verhouding = value / hoogste
+    r = 1 - verhouding
+
+    x.append(np.random.uniform(-r, r))
+    y.append(np.random.uniform(-r, r))
+    values.append(value)
+
+sizes = [value * 10000 for value in values]
+
+# Strepen tussen alle websites
+for i in range(len(x)):
+    for j in range(i + 1, len(x)):
+        plt.plot(
+            [x[i], x[j]],
+            [y[i], y[j]],
+            alpha=0.05,
+            linewidth=0.5
+        )
+
+# Websites
+plt.scatter(
+    x,
+    y,
+    s=sizes,
+    c=values,
+    cmap='Blues'
+)
+
+plt.colorbar(label='PageRank')
+plt.axis('equal')
+plt.axis('off')
 plt.show()

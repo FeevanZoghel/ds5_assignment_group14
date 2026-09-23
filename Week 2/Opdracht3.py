03cc475d9cd778aecf9d0b323a3ea8fc7c0e0265
MatthijsvanMeeuwen <579720@student.fontys.nl>
2026-09-23 13:57:55 +0200

    s

--- Week 2/Opdracht3.py
+++ Week 2/Opdracht3.py
@@ -1,48 +1,49 @@
-print('hoi')
-
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-import numpy as np
 import math as m
 
 
 def calculate_probabilities(G: nx.Graph) -> list:
     """
-    Berekent de kans dat elke node wordt geselecteerd
-    op basis van de degree van die node.
+    Bereken de selectiekans van iedere node op basis van zijn degree.
 
-    Fee
+    Een node met meer verbindingen krijgt een grotere kans om geselecteerd
+    te worden. De kansen staan in dezelfde volgorde als ``G.nodes()``.
+
+    Args:
+        G: De graaf waarvoor de selectiekansen worden berekend.
+
+    Returns:
+        Een lijst met kansen, waarvan de som 1 is.
+
+    Author:
+        Matthijs
     """
-    
-    total_degree = 0
 
-    for node in G.nodes():
-        total_degree += G.degree(node)
+    total_degree = sum(G.degree(node) for node in G.nodes())
 
-    probabilities = []
-
-    for node in G.nodes():
-        probability = G.degree(node) / total_degree
-        probabilities.append(probability)
-
-    return probabilities
+    return [
+        G.degree(node) / total_degree
+        for node in G.nodes()
+    ]
 
 
 def create_network(k : int, N: int, M: int) -> nx.Graph:
     """
-    Maakt een Barabási-Albert-netwerk.
+    Maak een ongerichte Barabási-Albert-graaf zonder graph generator.
 
+    Args:
+        k: Het aantal nodes dat in de start-star met node 1 wordt verbonden.
+        N: Het totale aantal nodes in de uiteindelijke graaf.
+        M: Het aantal bestaande nodes waaraan iedere nieuwe node wordt
+            verbonden.
 
-    Parameters:
-        k: Het aantal verbindingen voor de 1e node.
-        n: Het totale aantal nodes.
-        m: Het aantal verbindingen voor elke nieuwe node.
+    Returns:
+        De gegenereerde ongerichte graaf.
 
-    Return:
-    Het gegenereerde netwerk.
-
-    Fee
+    Author:
+        Matthijs
     """
     G = nx.Graph()
 
@@ -70,53 +71,42 @@ def create_network(k : int, N: int, M: int) -> nx.Graph:
         for chosen_node in chosen_nodes:
             G.add_edge(new_node, chosen_node)
 
-    # controleren op een self loop
+    # Controleer of er geen self-loop is ontstaan.
     for node in G.nodes():
-
         if G.has_edge(node, node):
-
             print(f"Self-loop gevonden bij node {node}")
-
 
     return G
 
 
+def visualize_network(G: nx.Graph) -> None:
+    """
+    Visualiseer de graaf met grootte en kleur gebaseerd op de degree.
 
+    Args:
+        G: Een graaf die door ``create_network`` is gemaakt.
 
+    Returns:
+        None. De visualisatie wordt in een matplotlib-venster getoond.
 
-
-def visualize_network(G):
-    '''
-    Visualiseert een Barabási-Albert-netwerk met een kleuren indicator en de size die is aangepast.
-
-    Parameters: 
-        G = Barabási-Albert-netwerk gemaakt met def create_network.
-
-    Return: 
-        Een plot van de Barabási-Albert-netwerk
-
-    Matthijs
-    '''
+    Author:
+        Matthijs
+    """
     N = len(G.nodes())
     sizes  = []
     colors = []
-    nodes = []
-
 
     for node in G.nodes():
         sizes.append(10*m.exp(-N/1000)*G.degree(node))
         colors.append(G.degree(node))
-        
-    
+
     plt.figure(figsize=(18, 14))
     pos = nx.spring_layout(G)
     nodes_plot = nx.draw_networkx_nodes(G,pos,node_size=sizes,node_color=colors,cmap="seismic")
     nx.draw_networkx_edges(G,pos, width = 0.3)
     plt.colorbar(nodes_plot)
     
-    return plt.show()
-
-
+    plt.show()
 
 G = create_network(5, 400, 4)
 visualize_network(G)
